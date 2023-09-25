e0a73f493be91be6ec02139bdde30737c8ab77bf
Sultonov557 <sultonovzerifboy@gmail.com>
2023-09-25 19:03:02 +0500

    .

--- backend/index.js
+++ backend/index.js
@@ -1,48 +1,22 @@
-const WebSocket = require("websocket").server;
-const http = require("http");
-const Redis = require("redis");
+const Server = require("ws").WebSocketServer;
+const db = require("./db");
 
 async function start() {
-	const server = http.createServer();
-	const db = {};
+	const ws = new Server({ port: 2668 });
 
-	const ws = new WebSocket({ httpServer: server, autoAcceptConnections: true });
-
-	const redis = Redis.createClient({
-		socket: {
-			host: "redis-17940.c10.us-east-1-3.ec2.cloud.redislabs.com",
-			port: 17940,
-		},
-		password: "z5572668",
-	});
-
-	redis.connect();
-
-	if ((await redis.get("message")) == undefined) {
-		console.log("messages init");
-		redis.set("message", "[]");
-		db.message = [];
-	} else {
-		db.message = JSON.parse(await redis.get("message"));
-	}
-
-	setInterval(async () => {
-		redis.set("message", JSON.stringify(db.message));
-		console.log("saved");
-	}, 30000);
-
-	server.listen(2668);
-
-	ws.on("connect", async (socket) => {
-		for (i in db.message) {
-			socket.send(db.message[i]);
-		}
-
+	ws.on("connection", async (socket) => {
 		socket.on("message", (data) => {
-			db.message.push(data.utf8Data);
-			socket.send(data.utf8Data);
+			data = data.toLocaleString();
+			db.set("message", Date.now(), data);
+			broadcast(data);
 		});
 	});
+
+	function broadcast(message) {
+		ws.clients.forEach((client, socket) => {
+			client.send(message);
+		});
+	}
 }
 
 module.exports = { start };
--- middleware/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res, next) => {
-	const cook = req.cookies;
-	if (cook == undefined) {
-		res.redirect("./auth");
-	}
-	next();
-};
