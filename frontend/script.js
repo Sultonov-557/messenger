712cf9c798118b1f6775032c0c89198749ef9aa3
Sultonov557 <sultonovzerifboy@gmail.com>
2023-09-25 19:05:21 +0500

    .

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket("ws://localhost:2668");
+const socket = new WebSocket("ws://" + document.domain + ":2668");
 
 const message = document.querySelector("span");
 const text = document.querySelector("input");
