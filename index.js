cf6facabecaf7b14d947681f17e495b2a70d7251
Sultonov557 <sultonovzerifboy@gmail.com>
2024-01-28 14:46:56 +0500

    reset

--- index.js
+++
@@ -1,6 +0,0 @@
-const express = require("express"); //importing express
-const app = express(); //making application
-app.listen(80); //listening application on port 80
-
-app.use(express.static(__dirname + "/frontend", { extensions: ["html"] })); //making frontend directory public on port 80
-require("./backend").start(); //starting backend
