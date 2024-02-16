f721cb0fdb8dd9adaab0d496a2c14696c707ceba
Sultonov557 <sultonovzerifboy@gmail.com>
2024-02-16 16:02:56 +0500

    .

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import SideBar from "../components/SideBar";
+
+export default function Home() {
+	return (
+		<main className="flex bg-background h-screen w-screen">
+			<SideBar />
+		</main>
+	);
+}
---
+++ frontend/src/components/SideBar.tsx
@@ -0,0 +1,3 @@
+export default function SideBar() {
+	return <div className="fixed bg-secondary top-0 left-0 h-screen w-16"></div>;
+}
