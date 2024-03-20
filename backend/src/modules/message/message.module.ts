46ff1bef447e5fd04f2e25fb97a8b6fd22db4f00
Sultonov-557 <sultonovzerifboy@gmail.com>
2024-03-20 15:02:07 +0500

    .

--- backend/src/modules/message/message.module.ts
+++ backend/src/modules/message/message.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Message } from './entities/message.entity';
 import { MessageGateway } from './message.gateway';
 import { User } from '../user/entities/user.entity';
+import { Group } from '../group/entities/group.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Message])],
+  imports: [TypeOrmModule.forFeature([User, Message, Group])],
   controllers: [],
   providers: [MessageService, MessageGateway],
 })
